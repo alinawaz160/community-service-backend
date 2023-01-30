06f3f7b45e15b6ac2e152b58edc54b4fd71f8fdb
AliNawaz <aleenawaz838@gmail.com>
2023-01-30 15:09:23 +0500

    sign up api creation stuck in login

--- app.js
+++ app.js
@@ -1,19 +1,85 @@
 const dotenv = require('dotenv');
 const express = require('express');
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const cookieParser= require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // Configuration ENV File and Require Connection File
-dotenv.config({path:"./config.env"});
-require("./db/conn")
-app.get('/', (req , res)=>{
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
+
+const port = process.env.PORT || 3000;
+
+//Require Model
+const Users = require("./models/userSchema");
+
+//using methods to get Data and cookies from Frontend
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
     res.send("Hello World")
 });
 
+//Registration
+app.post('/register', async (req, res) => {
+    try {
+        // Get body
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const createUser = new Users({
+            username: username,
+            email: email,
+            password: password
+        });
+        //Saving the created user...
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("Registered");
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+//Login User
+app.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        //Find User if exist
+        const user = await Users.findOne({ email: email});
+        if (user) {
+            //Verify password
+            const isMatch = await bcryptjs.compare(password, user.password);
+
+            if (isMatch) {
+                const token = await user.generteToken();
+                console.log(token);
+                res.cookie("jwt", token, {
+                    expires: new Date(Date.now() + 86400000),
+                    httpOnly: true
+                })
+                res.status(200).send("LoggedIn");
+                console.log("LoggedIn"); 
+            } else {
+                res.status(400).send("Invali Credentials")
+            }
+        } else {
+            res.status(400).send("Invalid Credentials");
+        }
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 //Run Server
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("Server is listening")
-} )
+})
--- models/userSchema.js
+++ models/userSchema.js
@@ -11,6 +11,44 @@ const userSchema = mongoose.Schema({
         unique : true
     },
     email:{
-        typ
-    }
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    tokens:[
+        {
+            token:{
+                type:String,
+                required:true
+            }
+        }
+    ]
 })
+//Hashing password to Secure
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        this.password = bcryptjs.hashSync(this.password,10);
+    }
+    next();
+})
+
+//Generate Tokens to verify user
+userSchema.methods.generateToken = async function(){
+    try{
+    let generatedToken = jwt.sign({_id : this.id} , youcanchooseanysecretkeyofcharacters);
+    this.tokens = this.tokens.concat({token : generatedToken});
+    await this.save();
+    return generatedToken;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+//Create Model
+const Users = new mongoose.model("USER", userSchema);
+module.exports = Users;
